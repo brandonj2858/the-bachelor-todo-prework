02940adc30c94deb256835be980310a22aa848a7
Brandon James <brandonj2858@gmail.com>
2019-07-22 14:38:53 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -43,5 +43,10 @@ end
 def get_average_age_for_season(data, season)
   total_age = 0
   num_of_people = 0
-  data[season].each do 
+  data[season].each do |person_hash|
+    total_age += person_hash["age"].to_i
+    num_of_people += 1
+  end
+  total_age
+end
  
