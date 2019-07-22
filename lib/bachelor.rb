48e59136bf3736ead16681fccbffec3461034dc9
Brandon James <brandonj2858@gmail.com>
2019-07-22 14:41:51 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -47,6 +47,6 @@ def get_average_age_for_season(data, season)
     total_age += person_hash["age"].to_i
     num_of_people += 1
   end
-  (total_age / num_of_people).round(0)
+  (total_age / num_of_people.to_f).round(0)
 end
  
