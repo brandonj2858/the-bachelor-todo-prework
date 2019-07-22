752f9a6036832a1a810e9bbf63cf8378d06746a8
Brandon James <brandonj2858@gmail.com>
2019-07-22 14:07:43 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -11,11 +11,7 @@ def get_contestant_name(data, occupation)
   data.each do |szn, person_hash|
     person_hash.each do |person_hash|
       if person_hash["occupation"] = occupation
-        return person_hash["name"]
-      end
-    end
-  end
-end
+ 
 
 def count_contestants_by_hometown(data, hometown)
   # code here
