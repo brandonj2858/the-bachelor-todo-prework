bdbb3abebd25e651fa9b4b9c5e17db51d7450339
Brandon James <brandonj2858@gmail.com>
2019-07-22 13:47:52 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ def get_first_name_of_season_winner(data, season)
   data.each do |szn_hash, person_hash|
     next unless szn_hash = season
     person_hash.each do |person|
-      winner_name = person[:"name"] if person[:"status"] = "Winner"
+      winner_name = person["name"] if person["status"] = "Winner"
     end
   end
   winner_name
