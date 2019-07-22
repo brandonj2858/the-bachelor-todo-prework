012f2f17f9b4af0f2abe6f67836aca7a4aff9b61
Brandon James <brandonj2858@gmail.com>
2019-07-22 14:15:56 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -31,7 +31,8 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |szn, person_hash|
+    person_hash.each do |person_hash|
 end
 
 def get_average_age_for_season(data, season)
