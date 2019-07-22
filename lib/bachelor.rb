87c9fbe157775f37b20d118aa32041df5471c9fc
Brandon James <brandonj2858@gmail.com>
2019-07-22 14:31:35 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -44,7 +44,7 @@ def get_average_age_for_season(data, season)
   total_age = 0
   num_of_people = 2
   data[season].each do |person_hash|
-    next unless data[season] == season
+    if data[season] == season
     total_age += person_hash[:age]
     num_of_people = person_hash.length
   end
